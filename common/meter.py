8350c505307d44656642dd7443a4fadc67321805
gkgo <91817417+gkgo@users.noreply.github.com>
2022-07-06 11:27:59 +0800

    Update meter.py

--- common/meter.py
+++ common/meter.py
@@ -1,5 +1,10 @@
 import math
+import warnings
+from typing import Optional
+
 import torch
+import torch.nn as nn
+from torch.nn.modules.loss import _Loss
 
 
 class Meter:
@@ -22,3 +27,61 @@ class Meter:
 
     def avg_and_confidence_interval(self):
         return self.avg(), self.confidence_interval()
+
+class PolyLoss(_Loss):
+    def __init__(self,
+                 softmax: bool = True,
+                 ce_weight: Optional[torch.Tensor] = None,
+                 reduction: str = 'mean',
+                 epsilon: float = 1.0,
+                 ) -> None:
+        super().__init__()
+        self.softmax = softmax
+        self.reduction = reduction
+        self.epsilon = epsilon
+        self.cross_entropy = nn.CrossEntropyLoss(weight=ce_weight, reduction='none')
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            input: the shape should be BNH[WD], where N is the number of classes.
+                You can pass logits or probabilities as input, if pass logit, must set softmax=True
+            target: if target is in one-hot format, its shape should be BNH[WD],
+                if it is not one-hot encoded, it should has shape B1H[WD] or BH[WD], where N is the number of classes,
+                It should contain binary values
+
+        Raises:
+            ValueError: When ``self.reduction`` is not one of ["mean", "sum", "none"].
+       """
+        if len(input.shape) - len(target.shape) == 1:
+            target = target.unsqueeze(1).long()
+        n_pred_ch, n_target_ch = input.shape[1], target.shape[1]
+        # target not in one-hot encode format, has shape B1H[WD]
+        if n_pred_ch != n_target_ch:
+            # squeeze out the channel dimension of size 1 to calculate ce loss
+            self.ce_loss = self.cross_entropy(input, torch.squeeze(target, dim=1).long())
+            # convert into one-hot format to calculate ce loss
+            target = to_one_hot(target, num_classes=n_pred_ch)
+        else:
+            # # target is in the one-hot format, convert to BH[WD] format to calculate ce loss
+            self.ce_loss = self.cross_entropy(input, torch.argmax(target, dim=1))
+
+        if self.softmax:
+            if n_pred_ch == 1:
+                warnings.warn("single channel prediction, `softmax=True` ignored.")
+            else:
+                input = torch.softmax(input, 1)
+
+        pt = (input * target).sum(dim=1)  # BH[WD]
+        poly_loss = self.ce_loss + self.epsilon * (1 - pt)
+
+        if self.reduction == 'mean':
+            polyl = torch.mean(poly_loss)  # the batch and channel average
+        elif self.reduction == 'sum':
+            polyl = torch.sum(poly_loss)  # sum over the batch and channel dims
+        elif self.reduction == 'none':
+            # BH[WD]
+            polyl = poly_loss
+        else:
+            raise ValueError(f'Unsupported reduction: {self.reduction}, available options are ["mean", "sum", "none"].')
+        return (polyl)
